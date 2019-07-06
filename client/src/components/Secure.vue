a121b3ea53c10b6208293b52587d6155c47c3209
KimHyeshin <hyeshin2530@gmail.com>
2019-07-06 22:16:33 +0900

    client vue init - vue init - add page

---
+++ client/src/components/Secure.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Secure
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Secure',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
