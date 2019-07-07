0b5f9870d9f67dc030b976749d14477b1a53372c
KimHyeshin <hyeshin2530@gmail.com>
2019-07-07 21:11:21 +0900

    server init

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+/**
+ * routing controllor
+ */
+app.use('/', require('./api/common'))
+
+app.listen(3030, () => {
+    // server connect
+    console.log('Example app listening on port 3030!')
+})
