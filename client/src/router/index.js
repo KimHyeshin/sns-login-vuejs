44830fb7a041b450b65ca24412b44cf6fbf867c8
KimHyeshin <hyeshin2530@gmail.com>
2019-09-24 00:22:05 +0900

    vue 스캐폴딩 추가 - 네비게이션 가드 삭제

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,24 +1,18 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 
-import HelloWorld from '@/components/HelloWorld'
-import Home from '@/pages/Home'
-import Login from '@/pages/Login'
-import Public from '@/pages/Public'
-import Secure from '@/pages/Secure'
+// views
+import Home from '@/views/Home'
+import Login from '@/views/Login'
+import Public from '@/views/Public'
+import Secure from '@/views/Secure'
+import Page404 from '@/views/Page404'
 
-import NotFound from '@/pages/NotFound'
+Vue.use(VueRouter);
 
-Vue.use(Router)
-
-// auth navigation guard
-const requireAuth = (to, from, next) => {
-  const isAuth = localStorage.getItem('token')
-  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
-  isAuth ? next() : next(loginPath)
-}
-
-export default new Router({
+export const router = new VueRouter({
+  mode: 'history',
+  linkActiveClass: 'open active',
   routes: [
     {
       path: '/',
@@ -38,17 +32,11 @@ export default new Router({
     {
       path: '/secure',
       name: 'Secure',
-      component: Secure,
-      beforeEnter: requireAuth
-    },
-    {
-      path: '/HelloWorld',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Secure
     },
     {
       path: '*',
-      component: NotFound
+      component: Page404
     }
   ]
-})
+});
